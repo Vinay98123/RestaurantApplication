cd14bcaa2ccf5968cc52a03de55c7e6ade530577
Dumal Bhaskar <dumalbhaskar@gmail.com>
2023-05-12 19:45:36 +0530

    updated project

--- Spring/src/main/java/com/example/demo/persistance/UserJPAInterfaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.persistance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import com.example.demo.entity.User;
-@RepositoryRestResource
-public interface UserJPAInterfaceRepository extends JpaRepository<User, Integer> {
-
-}
